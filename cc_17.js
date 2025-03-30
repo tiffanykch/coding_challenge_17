e68a1fc58ff9bd381cfeae031f2511f6cd693a66
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-03-30 12:56:26 -0400

    Task 2: Create a SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -1,5 +1,4 @@
 // TASK 1: CREATE CUSTOMER CLASS
-
 class Customer{
     constructor(name, email) {
         this.name = name;
@@ -20,7 +19,7 @@ class Customer{
     getTotalSpent() {
         let totalSpent = this.purchaseHistory.reduce((sum, total) => sum + total, 0);
         console.log(`Total Spend: $${totalSpent}`);
-        return totalSpent;        
+        return totalSpent;
     };
 }
 
@@ -28,11 +27,37 @@ class Customer{
 const customer1 = new Customer("Phineas Flynn","pflynn@disney.com");
 const customer2 = new Customer("Dr. Heinz Doofenshmirtz", "doof@doofenshmirtzevilinc.com");
 const customer3 = new Customer("Perry The Platypus", "pplatypus@owca.gov")
-// console.log(customer1);
-// console.log(customer2);
-// console.log(customer3);
 
 customer1.addPurchase(200);
 customer1.addPurchase(500);
 customer1.addPurchase(100);
 customer1.getTotalSpent();
+
+// TASK 2: CREATE A SALESREP CLASS
+class SalesRep{
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    };
+
+    addClient(customer) {
+        if(customer instanceof Customer) {
+            this.clients.push(customer);
+        }
+    };
+
+    getClientTotal(name) {
+        const client = this.clients.find(customer => customer.name === name);
+        
+        if (client) {
+            return client.getTotalSpent();
+        } else {
+            console.log("Client not found!");
+        }
+    }
+}
+
+// Test Cases
+const rep1 = new SalesRep("Mario")
+rep1.addClient(customer1);
+rep1.getClientTotal("Phineas Flynn");
